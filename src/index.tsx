2b52379c137c18527ccfcdd7c55fab48ccf6b56a
confucian <confucian.e@gmail.com>
2024-03-16 17:48:57 +0800

    feat: init frontend

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { FuelProvider } from "@fuel-wallet/react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <FuelProvider
+      fuelConfig={{
+        devMode: true,
+      }}
+    >
+      <App />
+    </FuelProvider>
   </React.StrictMode>
 );
 
